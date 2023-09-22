4b02e347584fc275d412262f9eddf4dc70d62f34
vimersiyanov2 <r@m.ru>
2023-09-22 10:50:45 +0300

    nid commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        String s1= "H1";
+        String s2=  new String("H1") ;
+        System.out.println(s1.equals(s2));
+    }
+}
